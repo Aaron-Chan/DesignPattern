05a8b6324e8765921467b2e995b17079212875a7
AaronChan <1903682265@qq.com>
2016-08-11 11:05:37 +0800

    增加Prototype的类图

--- Prototype/src/com/aaronchan/prototype/App.java
+++ Prototype/src/com/aaronchan/prototype/App.java
@@ -3,13 +3,26 @@ package com.aaronchan.prototype;
 public class App {
 
 	public static void main(String[] args) {
-		Toothpaste toothpaste = new DarlieToothpaste();
-		Shampoo shampoo = new RejoiceShampoo();
+		Toothpaste toothpaste;
+		Shampoo shampoo;
+		Shampoo produceShampoo;
+		Toothpaste produceToothpaste;
+		toothpaste = new DarlieToothpaste();
+		shampoo = new RejoiceShampoo();
 		CommodityFactory commodityFactory = new CommodityFactoryImp(toothpaste, shampoo);
-		Shampoo produceShampoo = commodityFactory.produceShampoo();
-		Toothpaste produceToothpaste = commodityFactory.produceToothpaste();
+		produceShampoo = commodityFactory.produceShampoo();
+		produceToothpaste = commodityFactory.produceToothpaste();
 		System.out.println(toothpaste == produceToothpaste);
 		System.out.println(shampoo == produceShampoo);
+
+		toothpaste = new CrestToothpaste();
+		shampoo = new SassonShampoo();
+		commodityFactory = new CommodityFactoryImp(toothpaste, shampoo);
+		produceShampoo = commodityFactory.produceShampoo();
+		produceToothpaste = commodityFactory.produceToothpaste();
+		System.out.println(toothpaste == produceToothpaste);
+		System.out.println(shampoo == produceShampoo);
+
 	}
 
 }
